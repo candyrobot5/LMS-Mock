ac595c9c630c4279cb803264e37b824eb8bed53c
tanaka hideaki <findwkwk@gmail.com>
2021-12-07 03:55:18 +0900

    feat: update couse-manage page functions

--- pages/playground/course-manage.vue
+++ pages/playground/course-manage.vue
@@ -12,8 +12,8 @@
         <b-row class="my-course" align-v="stretch">
           <!-- course body sidebar -->
           <b-col md="auto" align-self="stretch" class="my-course-sidebar">
-            <b-card class="h-100 bg-transparent">
-              <b-card class="h-100">
+            <b-card class="h-100 bg-transparent" body-class="p-2">
+              <b-card class="h-100" body-class="p-2">
                 <b-list-group>
                   <!-- course search -->
                   <b-list-group-item class="border-0 p-0">
@@ -45,7 +45,7 @@
           </b-col>
           <!-- course body main -->
           <b-col md="auto" align-self="stretch" class="my-course-main">
-            <b-card class="h-100 bg-transparent">
+            <b-card class="h-100 bg-transparent" body-class="p-2">
               <b-row>
                 <b-col cols="12">
                   <h4>{{ seletedCourseName }}</h4>
@@ -70,44 +70,98 @@
                   <label>ユーザー一覧</label>
                 </b-col>
               </b-row>
-              <b-row align-v="stretch" align-h="center">
-                <!-- unselect user list -->
-                <b-col align-self="stretch" class="d-flex flex-column">
-                  <b-card class="h-100">
-                    <b-list-group class="h-100">
-                      <b-list-group-item
-                        v-for="(user, uIdx) in unselectedUsersOptions"
-                        :key="uIdx"
-                        href="#"
-                        @click.prevent.stop="
-                          onCheckUnselectUser($event, user.groupId, user.id)
-                        "
-                      >
-                        <div>
-                          <b-form-group>
-                            <b-form-checkbox v-model="user.isChecked">
-                              {{ user.name }}
-                            </b-form-checkbox>
-                          </b-form-group>
+              <b-row align-v="start" align-h="center">
+                <b-col>
+                  <!-- unselected user list -->
+                  <b-row align-v="start" class="my-course-users__left">
+                    <b-col align-self="stretch">
+                      <b-card class="h-100" body-class="p-2">
+                        <!-- all checked button -->
+                        <div class="mb-2 text-right">
+                          <b-button
+                            class="py-1"
+                            @click="onAllCheckUnselectUsers"
+                          >
+                            <span class="small">全選択</span>
+                          </b-button>
                         </div>
-                      </b-list-group-item>
-                    </b-list-group>
-                  </b-card>
+                        <!-- list -->
+                        <b-list-group class="h-100">
+                          <b-list-group-item
+                            v-for="(user, uIdx) in unselectedUsersOptions"
+                            :key="uIdx"
+                            href="#"
+                            @click.prevent.stop="
+                              onCheckUnselectUser($event, user.groupId, user.id)
+                            "
+                          >
+                            <div>
+                              <b-form-group>
+                                <b-form-checkbox v-model="user.isChecked">
+                                  {{ user.name }}
+                                </b-form-checkbox>
+                              </b-form-group>
+                            </div>
+                          </b-list-group-item>
+                        </b-list-group>
+                      </b-card>
+                    </b-col>
+                  </b-row>
                 </b-col>
                 <!-- move button -->
                 <b-col cols="2" class="text-center">
-                  <div>
-                    <b-button block>
-                      <span>追加 &gt;&gt;</span>
-                    </b-button>
-                    <b-button block>
-                      <span>&lt;&lt; 削除</span>
-                    </b-button>
-                  </div>
+                  <b-row align-v="center" class="my-course-users__left">
+                    <b-col>
+                      <b-button
+                        block
+                        :disabled="!isCheckedUnselectedUsers"
+                        @click="onAddSelectedUser"
+                      >
+                        <span>追加 &gt;&gt;</span>
+                      </b-button>
+                      <b-button
+                        block
+                        :disabled="!isCheckedSelectedUsers"
+                        @click="onRemoveSelectedUser"
+                      >
+                        <span>&lt;&lt; 削除</span>
+                      </b-button>
+                    </b-col>
+                  </b-row>
                 </b-col>
-                <!-- selected user list -->
                 <b-col>
-                  <b-card class="h-100" />
+                  <!-- selected user list -->
+                  <b-row align-v="start" class="my-course-users__right">
+                    <b-col align-self="stretch">
+                      <b-card class="h-100" body-class="p-2">
+                        <!-- all checked button -->
+                        <div class="mb-2 text-right">
+                          <b-button class="py-1" @click="onAllCheckSelectUsers">
+                            <span class="small">全選択</span>
+                          </b-button>
+                        </div>
+                        <!-- list -->
+                        <b-list-group class="h-100">
+                          <b-list-group-item
+                            v-for="(user, uIdx) in selectedUsersOptions"
+                            :key="uIdx"
+                            href="#"
+                            @click.prevent.stop="
+                              onCheckSelectUser($event, user.groupId, user.id)
+                            "
+                          >
+                            <div>
+                              <b-form-group>
+                                <b-form-checkbox v-model="user.isChecked">
+                                  {{ user.name }}
+                                </b-form-checkbox>
+                              </b-form-group>
+                            </div>
+                          </b-list-group-item>
+                        </b-list-group>
+                      </b-card>
+                    </b-col>
+                  </b-row>
                 </b-col>
               </b-row>
             </b-card>
@@ -145,7 +199,7 @@ class CourseGroup {
   }
 }
 
-class CourseItem {
+class Course {
   constructor(id, name) {
     this.id = id
     this.name = name
@@ -157,9 +211,9 @@ export default {
   data() {
     return {
       courses: [
-        new CourseItem(1, 'コース1'),
-        new CourseItem(2, 'コース2'),
-        new CourseItem(3, 'コース3')
+        new Course(1, 'コース1'),
+        new Course(2, 'コース2'),
+        new Course(3, 'コース3')
       ],
       selectedCourseId: 1,
       groups: [
@@ -175,14 +229,21 @@ export default {
           new CourseUser(2, 'ユーザーB'),
           new CourseUser(3, 'ユーザーC'),
           new CourseUser(4, 'ユーザーD'),
-          new CourseUser(5, 'ユーザーE')
+          new CourseUser(5, 'ユーザーE'),
+          new CourseUser(6, 'ユーザーF'),
+          new CourseUser(7, 'ユーザーG')
         ]),
         new CourseGroup(3, 'グループ3', [
           new CourseUser(1, 'ユーザーあ'),
           new CourseUser(2, 'ユーザーい'),
           new CourseUser(3, 'ユーザーう'),
           new CourseUser(4, 'ユーザーえ'),
-          new CourseUser(5, 'ユーザーお')
+          new CourseUser(5, 'ユーザーお'),
+          new CourseUser(6, 'ユーザーか'),
+          new CourseUser(7, 'ユーザーき'),
+          new CourseUser(8, 'ユーザーく'),
+          new CourseUser(9, 'ユーザーけ'),
+          new CourseUser(10, 'ユーザーこ')
         ])
       ],
       selectedGroupId: 1,
@@ -213,12 +274,34 @@ export default {
         return user
       })
       return unselectedUsers
+    },
+    isCheckedUnselectedUsers() {
+      return (
+        this.unselectedUsersOptions.length > 0 &&
+        this.unselectedUsersOptions.some((user) => user.isChecked)
+      )
+    },
+    selectedUsersOptions() {
+      return this.selectedUsers
+    },
+    isCheckedSelectedUsers() {
+      return (
+        this.selectedUsersOptions.length > 0 &&
+        this.selectedUsersOptions.some((user) => user.isChecked)
+      )
     }
   },
   methods: {
+    /**
+     * コースの選択イベント
+     */
     onSelectCourse(event, courseId) {
       this.selectedCourseId = courseId
     },
+    /**
+     * 左ペインのユーザー一覧のチェックボックスのチェックイベント
+     *  チェックボックスのチェックを入れる/外す
+     */
     onCheckUnselectUser(event, groupId, userId) {
       const user = this.groups
         .find((group) => group.id === groupId)
@@ -226,6 +309,94 @@ export default {
       if (user) {
         user.isChecked = !user.isChecked
       }
+    },
+    /**
+     * 左ペインのユーザー一覧の全てのユーザーにチェックを入れる
+     */
+    onAllCheckUnselectUsers(event) {
+      this.unselectedUsersOptions.forEach((user) => {
+        user.isChecked = true
+      })
+    },
+    /**
+     * 右ペインのユーザー一覧のチェックボックスのチェックイベント
+     *  チェックボックスのチェックを入れる/外す
+     */
+    onCheckSelectUser(event, groupId, userId) {
+      const user = this.selectedUsers.find(
+        (user) => user.groupId === groupId && user.id === userId
+      )
+      if (user) {
+        user.isChecked = !user.isChecked
+      }
+    },
+    /**
+     * 右ペインのユーザー一覧の全てのユーザーにチェックを入れる
+     */
+    onAllCheckSelectUsers(event) {
+      this.selectedUsers.forEach((user) => {
+        user.isChecked = true
+      })
+    },
+    /**
+     * 選択済みユーザー一覧に追加する
+     */
+    onAddSelectedUser(event) {
+      // 右ペインに追加する
+      const checkedUsers = this.unselectedUsersOptions
+        .filter((user) => user.isChecked)
+        .map((user) => {
+          const u = { ...user }
+          u.isChecked = false
+          return u
+        })
+      const selectedUsers = this.selectedUsers.concat(checkedUsers)
+      this.selectedUsers = selectedUsers
+      // 左ペインのチェックが入っているユーザーを削除する
+      const group = this.groups.find(
+        (group) => group.id === this.selectedGroupId
+      )
+      if (group) {
+        const uncheckedUsers = this.unselectedUsersOptions
+          .filter((user) => !user.isChecked)
+          .map((user) => {
+            const u = { ...user }
+            u.isChecked = false
+            return u
+          })
+        group.users = uncheckedUsers
+      }
+    },
+    /**
+     * 選択済みユーザー一覧から削除する
+     */
+    onRemoveSelectedUser(event) {
+      // 左ペインに元に戻す
+      this.selectedUsers
+        .filter((user) => user.isChecked)
+        .map((user) => {
+          const u = { ...user }
+          u.isChecked = false
+          return u
+        })
+        .forEach((user) => {
+          const group = this.groups.find((group) => group.id === user.groupId)
+          if (group) {
+            const u = { ...user }
+            delete u.groupId
+            group.users.push(u)
+            group.users.sort((aUser, bUser) => aUser.id - bUser.id)
+          }
+        })
+      // 右ペインのチェックが入っているユーザーを削除する
+      const uncheckedUsers = this.selectedUsers
+        .filter((user) => !user.isChecked)
+        .map((user) => {
+          const u = { ...user }
+          u.isChecked = false
+          return u
+        })
+      this.selectedUsers = uncheckedUsers
     }
   }
 }
@@ -245,6 +416,17 @@ $my-course-sidebar-width: 350px;
   /* .my-course-main */
   &-main {
     width: calc(100% - $my-course-sidebar-width);
+  }
+  /* .my-course-users */
+  &-users {
+    /* .my-course-users__left */
+    &__left {
+      min-height: 100vh;
+    }
+    /* .my-course-users__right */
+    &__right {
+      min-height: 100vh;
+    }
   }
 }
 </style>
